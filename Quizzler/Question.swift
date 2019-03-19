2fa42042b720c19160b9b3ddd927593a643db63b
Nicholas Repaci <nickrepaci@gmail.com>
2019-03-19 18:21:45 -0400

    Initial commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,23 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Nick Repaci on 2/4/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+        
+    }
+    
+}
+
